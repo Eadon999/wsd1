ce8faf12515f1826b2e9e6fd851dfaeee9e4c2ae
sff1019 <starfield1019@gmail.com>
2018-12-11 12:15:10 +0900

    Fix format

--- format.py
+++ format.py
@@ -4,31 +4,46 @@ import json
 import csv
 
 
-args = sys.argv
-directory = './jsons_premium'
-output = './formatted.csv'
-write_header = True
-header = []
+def split_dictionary(dictionaries):
+    result = []
+    for dictionary in dictionaries:
+        item = []
+        for key in dictionary:
+            item.append(dictionary[key])
+        result.append(item)
 
-for index, path in enumerate(os.listdir(directory)):
+    return result
 
-    with open(directory + '/' + path, 'r') as f:
-        data = json.load(f)
 
-    row = []
-    for key in data:
-        if index == 0:
-            header.append(key)
-        row.append(data[key])
+if __name__ == '__main__':
+    args = sys.argv
+    directory = './jsons_premium'
+    output = './formatted.csv'
+    write_header = True
+    header = []
 
-    if not os.path.isfile(output):
-        with open(output, 'w') as f:
-            writer = csv.writer(f, lineterminator='\n')
-            writer.writerow(header)
-            writer.writerow(row)
-    else:
-        with open(output, 'a') as f:
-            writer = csv.writer(f, lineterminator='\n')
-            writer.writerow(row)
+    for index, path in enumerate(os.listdir(directory)):
 
-    print(f'Row {index} done!')
+        with open(directory + '/' + path, 'r') as f:
+            data = json.load(f)
+
+        row = []
+        for key in data:
+            if index == 0:
+                header.append(key)
+            if key == 'recipeIngredient':
+                row.append(split_dictionary(data[key]))
+            else:
+                row.append(data[key])
+
+        if not os.path.isfile(output):
+            with open(output, 'w') as f:
+                writer = csv.writer(f, lineterminator='\n')
+                writer.writerow(header)
+                writer.writerow(row)
+        else:
+            with open(output, 'a') as f:
+                writer = csv.writer(f, lineterminator='\n')
+                writer.writerow(row)
+
+        print(f'Row {index} done!')
