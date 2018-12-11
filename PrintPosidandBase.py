eef058ca748371cc3e7836d1b17fc5f128445e09
kk3453 <ken190153@gmail.com>
2018-12-11 19:21:55 +0900

    defした

--- PrintPosidandBase.py
+++ PrintPosidandBase.py
@@ -2,14 +2,19 @@ import sys
 import pandas as pd
 import MeCab
 
-args = sys.argv
-s = args[1]
+def StrToPosidAndBase(s):
+    # neologdの辞書のパスを変えたりしてみてください
+    # neologd内のdicrcに以下を加えてください
+    # ; test
+    # node-format-test = %h/%f[6]\n
+    # eos-format-test = EOS\n
+    m = MeCab.Tagger("-Otest -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd/")
+    s = m.parse(s)
+    return s
 
-# neologdの辞書のパスを変えたりしてみてください
-# neologd内のdicrcに以下を加えてください
-# ; test
-# node-format-test = %h/%f[6]\n
-# eos-format-test = EOS\n
-m = MeCab.Tagger("-Otest -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd/")
+if __name__ == '__main__':
+    args = sys.argv
+    s = args[1]
+    s = StrToPosidAndBase(s)
+    print(s)
 
-print(m.parse(s))
