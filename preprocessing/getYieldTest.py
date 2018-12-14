81de4ea19c1bdead0aa040fc56b2558d012d5690
sff1019 <starfield1019@gmail.com>
2018-12-14 16:11:39 +0900

    Fix kanji to english numbers

--- preprocessing/getYieldTest.py
+++ preprocessing/getYieldTest.py
@@ -1,5 +1,5 @@
 # テスト実行例
-from .. import recipeYieldFilter as ryf
+import recipeYieldFilter as ryf
 import pandas as pd
 import sys
 
@@ -13,12 +13,12 @@ df = pd.read_csv(
     usecols=[
         'recipeYield',  # 何人分
     ],
-    nrows=64,  # 行数
+    nrows=100,  # 行数
 )
 # print(df)
 # print(type(df.iat[0, 0]))  # 型
 
 df = pd.concat([df, ryf.getYield(df.recipeYield)], axis=1)
 print(df)
-print(type(df.iat[0, 0]))
-print(type(df.iat[0, 1]))
+# print(type(df.iat[0, 0]))
+# print(type(df.iat[0, 1]))
