4cc009b9e53655fb30d2f5b2fa9e990d0515f12b
sff1019 <starfield1019@gmail.com>
2018-12-17 22:46:13 +0900

    Divide ingredients and amount

--- data/format_individual.py
+++ data/format_individual.py
@@ -9,30 +9,44 @@ directory = './jsons_premium'
 write_header = True
 header = []
 
-for index, path in enumerate(os.listdir(directory)):
 
-    with open(directory + '/' + path, 'r') as f:
-        data = json.load(f)
+def split_dictionary(dictionaries):
+    ingredients = [dictionary[key]
+                   for dictionary in dictionaries for key in dictionary if key == 'name']
+    amount = [dictionary[key]
+              for dictionary in dictionaries for key in dictionary if key == 'quantity']
 
-    for key in data:
-        output = f'./{key}_output.csv'
-        input = []
-        if index == 0:
-            header = [key]
+    return ingredients, amount
 
-        if data[key] is not None:
-            input.append(data[key])
-        else:
-            input.append('N/A')
 
-        if not os.path.isfile(output):
-            with open(output, 'w') as f:
-                writer = csv.writer(f, lineterminator='\n')
-                writer.writerow(header)
-                writer.writerow(input)
-        else:
-            with open(output, 'a') as f:
-                writer = csv.writer(f, lineterminator='\n')
-                writer.writerow(input)
+if __name__ == '__main__':
+    for index, path in enumerate(os.listdir(directory)):
+        with open(directory + '/' + path, 'r') as f:
+            data = json.load(f)
 
-    print(f'Row {index} done!')
+        for key in data:
+            output = f'./{key}_output.csv'
+            row = []
+            if index == 0:
+                header = [key]
+
+            if key == 'recipeIngredient':
+                ingredients, amount = split_dictionary(data[key])
+                row.append(ingredients)
+                row.append(amount)
+            elif data[key] is not None:
+                row.append(data[key])
+            else:
+                row.append('N/A')
+
+            if not os.path.isfile(output):
+                with open(output, 'w') as f:
+                    writer = csv.writer(f, lineterminator='\n')
+                    writer.writerow(header)
+                    writer.writerow(row)
+            else:
+                with open(output, 'a') as f:
+                    writer = csv.writer(f, lineterminator='\n')
+                    writer.writerow(row)
+
+        print(f'Row {index} done!')
