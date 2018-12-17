2c14e3580808d25e4e7fa26c4f5adc5bcd15cb22
Keita <45677329+keitavt@users.noreply.github.com>
2018-12-17 10:25:36 +0900

    カタカナ読みテスト

---
+++ ingredient_search/janomeTest.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from janome.charfilter import *
+from janome.tokenizer import Tokenizer
+from janome.tokenfilter import *
+from janome.analyzer import Analyzer
+
+args = sys.argv
+line = args[1]
+
+cf = []
+t  = Tokenizer()
+tf = []
+a  = Analyzer(cf, t, tf)
+
+print(' (1) 入力: ')
+print(line)
+
+for token in a.analyze(line):
+    print(' (2) Token オブジェクト: ')
+    print(token)
+    
+    print(' (3) 読み: ')
+    print(token.reading)
