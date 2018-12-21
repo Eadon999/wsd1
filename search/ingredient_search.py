dff30a496625526bb9517b9a6097f224c6c77144
sff1019 <starfield1019@gmail.com>
2018-12-21 16:06:29 +0900

    完全一致でなくても検索可能に変化

--- search/ingredient_search.py
+++ search/ingredient_search.py
@@ -9,8 +9,9 @@ mecab = MeCab.Tagger('-Oyomi')
 
 def search_target(target, id_list, yomi_list):
     for index, ingredients in enumerate(yomi_list):
-        if target in ingredients:
-            print(id_list[index])
+        for item in ingredients:
+            if target in item:
+                print(f'{id_list[index]} : {item}')
 
 
 if __name__ == '__main__':
@@ -24,17 +25,12 @@ if __name__ == '__main__':
         data_path,
         usecols=[
             'recipe_id',
-            'ingredients',
             'ingredients_yomi',
-            # 'amount'
         ],
-        # nrows=1,  # 行数
         dtype=str,
     )
 
     recipe_id = df.recipe_id.values.tolist()
-    # ingredients_list = [ast.literal_eval(column)
-    #                     for column in df.ingredients]
     yomi_list = [ast.literal_eval(column) for column in df.ingredients_yomi]
 
     search_target(target, recipe_id, yomi_list)
