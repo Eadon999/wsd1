253a90b1a36d5a091e74fc171d12b082e8539ab2
sff1019 <starfield1019@gmail.com>
2018-12-21 16:05:01 +0900

    大匙、小匙を追加

--- preprocessing/filter_readings.py
+++ preprocessing/filter_readings.py
@@ -3,7 +3,15 @@ import MeCab
 mecab = MeCab.Tagger('-Oyomi')
 
 
-def get_readings_list(data):
+def get_readings_array(data):
     result = [[mecab.parse(item).replace('\n', '')
                for item in lst] for lst in data]
     return result
+
+
+def get_reading_list(data):
+    return [mecab.parse(item).replace('\n', '') for item in data]
+
+
+def get_reading(word):
+    return mecab.parse(word).replace('\n', '')
