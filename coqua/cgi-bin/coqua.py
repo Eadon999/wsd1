e1f1d549d4b9c0e47b4353355365103844ddcdff
nymwa <nymwa0@gmail.com>
2018-12-27 20:04:10 +0900

    coqua

--- coqua/cgi-bin/coqua.py
+++ coqua/cgi-bin/coqua.py
@@ -17,7 +17,8 @@ for key in form.keys():
 
 box = ''
 if 'text' in dct:
-	lst = dct['text'].split()
+	txt = dct['text']
+	lst = txt.split()
 	Alst = []
 	Nlst = []
 	for x in lst:
@@ -27,12 +28,12 @@ if 'text' in dct:
 			Nlst.append(x[1:])
 	cdb = coquadb.CoquaDB('coqua.db')
 	lst = cdb.ingredients_list(Alst, Nlst)
+	box = '「' + txt + '」 ' + str(len(lst)) + '件の検索結果'
 	for num in lst:
 		box += "<p><a href = https://cookpad.com/recipe/" + str(num) + ">";
 		box += cdb.name(num)[0] + "</a></p>"
 else:
-	box = "error"
-	box = str(dct)
+	box += '材料名を入れて検索してみましょう'
 
 print('Content-type: text/html\nAccess-Control-Allow-Origin: *\n')
 print(box)
