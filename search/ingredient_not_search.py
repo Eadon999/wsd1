475c44ff977fbadadc670f54c240babc45c7127c
sff1019 <starfield1019@gmail.com>
2018-12-28 12:17:18 +0900

    Fix indentation

--- search/ingredient_not_search.py
+++ search/ingredient_not_search.py
@@ -11,25 +11,25 @@ mecab = MeCab.Tagger('-Oyomi')
 
 def search_target(target, id_list, yomi_list):
     index_list = []  # 行番号リスト
-    
+
     for index, ingredients in enumerate(yomi_list):
         for item in ingredients:
             if target in item:
                 print(f'{id_list[index]} : {item}')
                 index_list.append(index)  # 部分一致したら行番号を追加
-                
+
     print('\nHit: ' + str(index_list) + '\n')  # 確認用
-    
+
     return index_list  # 検索にヒットしたレシピの行番号を返す
 
 
 def print_id_and_yomi_list(index_list, id_list, yomi_list):
     index_list.sort()  # 行番号を昇順に整列
-    
+
     print('\n検索結果 (' + str(len(index_list)) + ' 件) ')
     for i in index_list:
         print(f'{i:4}: {id_list[i]:7}: {yomi_list[i]}')  # 各行を出力
-        
+
     return index_list  # 整列済みの行番号リストを返す
 
 
@@ -64,15 +64,15 @@ if __name__ == '__main__':
 
     # 検索したい語にヒットしたレシピの行番号
     result_t = search_target(target, recipe_id, yomi_list)
-    
+
     if str0:
         # 検索結果から除外したい語にヒットしたレシピの行番号
         result_e = search_target(exception, recipe_id, yomi_list)
-        
-        # 最終結果: リストの差 (差集合から) 
+
+        # 最終結果: リストの差 (差集合から)
         final_result = op.l_diff(result_t, result_e)
     else:
         final_result = result_t
-    
+
     # 最終結果を出力
     print_id_and_yomi_list(final_result, recipe_id, yomi_list)
