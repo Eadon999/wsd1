6fc9bb4a0a90a2514884f09d3bf55f884f791ca1
nymwa <nymwa0@gmail.com>
2018-12-30 23:51:28 +0900

    coqua

--- coqua/cgi-bin/coqua.py
+++ coqua/cgi-bin/coqua.py
@@ -5,23 +5,24 @@ import cgi
 import cgitb
 import coquadb
 
-def make_page(num_split):
-		return '<div class = "page">' + ''.join(F'<a href = "#cont{x}">{x}</a> ' for x in range(1, num_split+1)) + '</div>'
+def make_page(num_split, n):
+	tmp = [F'<a class = "cont{x}" href = "#cont{x}">{x}</a> ' if x != n else F'<b>{x} </b>' for x in range(1, num_split+1)]
+	return '<div class = "page">' + ''.join(tmp) + '</div>'
 
 
 def make_link(lst):
-		tmp = [F'<p><img src = "{url}" width = 100px><a href = https://cookpad.com/recipe/{num}>{name}</a></p>' for [num, name, url] in lst]
-		return '<div class = "link">' + ''.join(tmp) + '</div>'
+	tmp = [F'<p><img src = "{url}" height = 141px width = 100px><a href = https://cookpad.com/recipe/{num}>{name}</a></p>' for [num, name, url] in lst]
+	return '<div class = "link">' + ''.join(tmp) + '</div>'
 
 
 def get_cont(lst):
 	len_split = 10
-	num_split = int(len(lst)/len_split)
+	num_split = int((len(lst)-1)/len_split)+1
 	lsts = [lst[i : i+len_split] for i in range(0, len(lst), len_split)]
 	tmp = '<div class = "cont">'
 	for i, lst in zip(range(1, num_split+1), lsts):
 		tmp += F'<div id = "cont{i}">' if i == 1 else F'<div id = "cont{i}" style = "display:none;">'
-		tmp += make_page(num_split) + make_link(lst) + make_page(num_split)
+		tmp += make_page(num_split, i) + make_link(lst) + make_page(num_split, i)
 		tmp += '</div>'
 	tmp += '</div>'
 	return tmp
