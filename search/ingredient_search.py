bc2d2b0ca78648d952ff51672d6a6eb4a1e0a5b9
sff1019 <starfield1019@gmail.com>
2019-01-11 10:54:28 +0900

    余剰検索リスト

--- search/ingredient_search.py
+++ search/ingredient_search.py
@@ -18,19 +18,30 @@ if __name__ == '__main__':
         usecols=[
             'recipe_id',
             'ingredients_yomi',
+            'amount',
         ],
         dtype=str,
     )
 
     recipe_id = df.recipe_id.values.tolist()
     yomi_list = [ast.literal_eval(column) for column in df.ingredients_yomi]
+    amount_list = [ast.literal_eval(column) for column in df.amount]
 
     include = input('検索する材料名をスペース区切りで入力してください： ')
     exclude = input('検索から省く材料名をスペース区切りで入力してください： ')
+    chck_amount = input('余剰材料検索を行いますか？(y / n)： ')
 
     # 文字列をリストに変換
     include = formatter.convert_str_to_list(include, ' ')
     exclude = formatter.convert_str_to_list(exclude, ' ')
 
-    result = search_engine.get_ids(yomi_list, recipe_id, include, exclude)
-    print(result)
+    indexes = search_engine.get_indexes(yomi_list, recipe_id, include, exclude)
+    ids = search_engine.get_ids(recipe_id, indexes)
+
+    if chck_amount == 'y':
+        pair = search_engine.get_pairing_element(
+            include, ids, yomi_list, amount_list, indexes)
+        print(pair)
+        search_engine.get_combination_sum(2, include, pair)
+    else:
+        print(ids)
--- search/modules/search_engine.py
+++ search/modules/search_engine.py
@@ -1,4 +1,5 @@
 import MeCab
+import itertools
 
 
 mecab = MeCab.Tagger('-Oyomi -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
@@ -16,11 +17,21 @@ class SearchEngine(object):
                   and not self.check_list_similarity(exclude, lst)]
         return result
 
-    def get_ids(self, readings, ids, include, exclude):
-        result = [ids[index]
+    def get_indexes(self, readings, ids, include, exclude):
+        """
+        検索にヒットしたレシピのインデックスをリストで返す関数
+        """
+        result = [index
                   for index, lst in enumerate(readings)
                   if self.check_list_included(include, lst)
                   and not self.check_list_similarity(exclude, lst)]
+        return result
+
+    def get_ids(self, ids, indexes):
+        """
+        index番目のidをリストとして返す
+        """
+        result = [ids[index] for index in indexes]
         return result
 
     def get_target_readings(self, word):
@@ -44,3 +55,38 @@ class SearchEngine(object):
         含まれない：False
         """
         return set(lst1).issubset(set(lst2))
+
+    def get_pairing_element(self, target, id_lst, lst1, lst2, recipe_index):
+        """
+        lst_1のリストの同じインデックスのlst_2の要素を取り出す
+        return: [{'item': {'id': 'amount', ...}, ...}]
+        """
+        result = {
+            item: {
+                id_lst[i]: lst2[num][lst1[num].index(item)]
+                for i, num in enumerate(recipe_index)
+            } for item in target
+        }
+
+        return result
+
+    def get_combination_sum(self, amount, ingredient, data):
+        """
+        data内の指定された材料がamountになるように計算し、その組み合わせを出力する
+        """
+        for item in ingredient:
+            amounts = list(data.get(item).values())
+        for index, amount in enumerate(amounts):
+            if isinstance(amount, str):
+                amounts[index] = 999
+        print(amounts)
+        # print(list(self.subset_sum(amounts, 3)))
+
+    def subset_sum(self, numbers, target, partial=[], partial_sum=0):
+        if partial_sum == target:
+            yield partial
+        if partial_sum >= target:
+            return
+        for i, n in enumerate(numbers):
+            remaining = numbers[i + 1:]
+            yield from self.subset_sum(remaining, target, partial + [n], partial_sum + n)
