a96988da97f4b1b789b30217a3b25f73d0e9cd8b
nymwa <nymwa0@gmail.com>
2019-01-25 10:25:06 +0900

    coqua

--- coqua/cgi-bin/renewdb.py
+++ coqua/cgi-bin/renewdb.py
@@ -2,6 +2,7 @@ import json
 import glob
 import sqlite3
 import datetime
+import coquadb
 import pandas as pd
 import ast
 
@@ -24,7 +25,7 @@ def init_tables(cdb): # テーブルの初期化
 	cdb.execute("CREATE INDEX idx_publication_recipe_id ON publications(recipe_id)")
 	# ingredients (材料名)
 	cdb.drop('ingredients')
-	cdb.execute("CREATE TABLE ingredients (recipe_id INTEGER, name TEXT, pron TEXT, amount TEXT)")
+	cdb.execute("CREATE TABLE ingredients (recipe_id INTEGER, name TEXT, pron TEXT, amount TEXT, converted REAL, unit TEXT)")
 	cdb.execute("CREATE INDEX idx_ingredient_recipe_id ON ingredients(recipe_id)")
 	cdb.execute("CREATE INDEX idx_ingredient_name ON ingredients(name)")
 	cdb.execute("CREATE INDEX idx_ingredient_pron ON ingredients(pron)")
@@ -41,7 +42,7 @@ def init_tables(cdb): # テーブルの初期化
 
 def insert_records(cdb):
 	# jsonファイルから
-	jlst = glob.glob("../../recipe0115/*")
+	jlst = glob.glob("../../../recipe0115/*")
 	for x in jlst:
 		with open(x) as f:
 			j = json.load(f)
@@ -56,19 +57,29 @@ def insert_records(cdb):
 			date = datetime.datetime.strptime(j['datePublished'], '%Y-%m-%d')
 			cdb.execute('INSERT INTO publications values(%s, "%s", %s, %s, %s, %s)'
 				% (rid, j['datePublished'], str(date.year), str(date.month), str(date.day), str((date - datetime.datetime(2000,1,1,0,0)).days)))
+			# cooktimes(調理時間)
+			cdb.execute('INSERT INTO cooktimes values(%s, %s)' % (rid, j['cooktime'][2:-2]))
 	# 各CSVファイルから
-	# ingredients(材料)
-	df = pd.read_csv("../preprocessed/filtered_ingredient.csv")
+	# ingredients(材料) yields
+	#df = pd.read_csv("../preprocessed/filtered_ingredient.csv")
+	#for i, row in df.iterrows():
+	#	lst = list(zip(
+	#		ast.literal_eval(row['ingredients']),
+	#		ast.literal_eval(row['ingredients_yomi']),
+	#		ast.literal_eval(row['amount'])))
+	#	for tup in lst:
+	#		cdb.execute("INSERT INTO ingredients values(" + str(row['recipe_id']) + ','
+	#				+ "'" + str(tup[0]) + "','" + str(tup[1]) + "','" + str(tup[2]).replace("'",'').replace('"','') + "')")
+	#		if pd.isnull(row['servings']) == False:
+	#			cdb.execute("INSERT INTO yields values(" + str(row['recipe_id']) + ',' + str(row['servings']) + ")")
+	df = pd.read_csv("../preprocessed/db_ingredients.csv")
 	for i, row in df.iterrows():
-		lst = list(zip(
-			ast.literal_eval(row['ingredients']),
-			ast.literal_eval(row['ingredients_yomi']),
-			ast.literal_eval(row['amount'])))
-		for tup in lst:
-			cdb.execute("INSERT INTO ingredients values(" + str(row['recipe_id']) + ','
-					+ "'" + str(tup[0]) + "','" + str(tup[1]) + "','" + str(tup[2]) + "')")
-			if pd.isnull(row['servings']) == False:
-				cdb.execute("INSERT INTO yields values(" + str(row['recipe_id']) + ',' + str(row['servings']) + ")")
+		cdb.execute('INSERT INTO ingredients values(%s, "%s", "%s", "%s", %s, %s)' %
+				(row['recipe_id'], row['name'], row['pron'], row['amount'],
+					"null" if pd.isnull(row['converted']) else row['converted'],
+					"null" if pd.isnull(row['unit'])      else '"' + str(row['unit'])  + '"'))
+		if pd.isnull(row['servings']) == False:
+			cdb.execute("INSERT INTO yields values(" + str(row['recipe_id']) + ',' + str(row['servings']) + ")")
 
 
 def renewdb(cdb):
@@ -78,7 +89,7 @@ def renewdb(cdb):
 	insert_records(cdb)
 
 if __name__ == '__main__':
-	cdb = __import__('cgi-bin.coquadb').coquadb.CoquaDB('coqua.db')
+	cdb = coquadb.CoquaDB('coqua.db')
 	cdb.debug_mode(True)
 	renewdb(cdb)
 	cdb.commit()
